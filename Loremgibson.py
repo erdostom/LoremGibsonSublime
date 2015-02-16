5e3ab5fd860e9ba3127b716d5485e7366dbe83d3
Tamas Erdos <tamas@tamaserdos.com>
2015-02-16 16:46:01 -0500

    removing the scraping

--- Loremgibson.py
+++ Loremgibson.py
@@ -1,27 +1,29 @@
 import sublime
 import sublime_plugin
-import re
 import random
-import urllib
 
 
 class LoremgibsonCommand(sublime_plugin.TextCommand):
+
+
     @classmethod
-    def scrapesite(self):
-        myText = []
-        raw = urllib.request.urlopen("http://loremgibson.com").read().decode("utf-8")
-        regex1 = re.compile("myText\[\d+\].*;")
-        var_lines = regex1.findall(raw)
-        regex2 = re.compile("myText\[\d+?\] = \"([a-zA-Z- ]+)\";")
-        for line in var_lines:
-            r = regex2.search(line.strip('\n'))
-            if r:
-                item = r.groups()[0]
-                myText.append(item)
-        random.shuffle(myText)
-        out = " ".join(myText[0:72])
-        return out
+    def generate_sentence(self, length=5):
+        basetext = ['concrete', 'face forwards', 'hotdog', 'futurity', 'motion', 'bridge', 'drugs', 'realism', 'dome', 'sign', 'A.I.', 'warehouse', 'bomb', 'receding', 'fluidity', 'franchise', 'towards', 'neon', 'rifle', 'kanji', 'saturation point', 'ablative', 'skyscraper', 'car', 'pen', 'soul-delay', 'tower', '-space', '-ware', 'digital', 'wristwatch', 'advert', 'boat', 'jeans', 'physical', 'DIY', 'sentient', 'render-farm', 'city', 'corporation', 'table', 'free-market', 'Chiba', 'smart-', 'Shibuya', 'silent', 'numinous', 'semiotics', 'girl', 'assault', 'drone', 'refrigerator', 'euro-pop', 'savant', 'uplink', 'j-pop', 'grenade', 'computer', 'youtube', 'knife', 'artisanal', 'RAF', 'rebar', 'apophenia', 'tube', 'sensory', 'San Francisco', 'garage', 'market', 'vinyl', 'cartel', 'meta-', 'katana', 'rain', 'corrupted', 'range-rover', 'hacker', 'narrative', 'shanty town', 'Legba', 'faded', 'systema', 'nano-', 'bicycle', 'nodal point', 'monofilament', 'order-flow', 'footage', 'long-chain hydrocarbons', 'papier-mache', 'nodality', 'crypto-', 'engine', 'stimulate', 'lights', 'carbon', 'singularity', 'modem', 'denim', 'math-', 'plastic', 'fetishism', 'urban', 'sub-orbital', 'dead', 'Kowloon', 'paranoid', 'sunglasses', 'voodoo god', 'pre-', 'pistol', 'gang', '3D-printed', 'courier', 'construct', 'vehicle', 'sprawl', 'marketing', 'augmented reality', 'Tokyo', 'tanto', 'dolphin', 'chrome', 'office', 'neural', '8-bit', 'film', 'shrine', 'network', 'beef noodles', 'post-', 'woman', 'military-grade', 'tattoo', 'into', 'cyber-', 'man', 'disposable', 'spook', 'wonton soup', 'tiger-team', 'decay', 'weathered', 'tank-traps', 'media', 'assassin', 'industrial grade', 'systemic', 'BASE jump', 'convenience store', 'camera', 'cardboard', 'alcohol', 'claymore mine', 'shoes', 'boy', 'otaku', 'human', 'geodesic', 'dissident']
+        random.shuffle(basetext)
+        sentence = basetext[0:length]
+        sentence[0] = sentence[0][0].upper() + sentence[0][1:]  # upcase first char of sentence
+        sentence[-1] = sentence[-1] + '.'  # adding some punctuation
+        sentence = ' '.join(sentence)
+        return sentence
+
+    @classmethod
+    def generate_paragraph(self, length=4):
+        paragraph = []
+        for i in range(length):
+            paragraph.append(LoremgibsonCommand.generate_sentence(random.randint(7, 17)))
+        return ' '.join(paragraph)
+
 
     def run(self, edit):
-        out = LoremgibsonCommand.scrapesite()
+        out = LoremgibsonCommand.generate_paragraph(random.randint(3, 9))
         self.view.insert(edit, self.view.sel()[0].begin(), out)
